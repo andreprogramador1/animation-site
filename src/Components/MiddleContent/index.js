3ee272319da444be9fa575fd4d8f6e53effee7e9
André ponce <andre_epr@outlook.com>
2020-12-14 01:50:35 -0300

    middle content adjusts

--- src/Components/MiddleContent/index.js
+++ src/Components/MiddleContent/index.js
@@ -26,138 +26,35 @@ export const MiddleContent = () => {
         <img src="https://www.aplusc.tv/assets/uploads/_sideHalfImage/Website-Main-Page_05.jpg" alt="image"/>
       </div>
       <div className='content'>
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
 
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
+        <h1><br/><br/></h1>
+        <h2>We're A+C, a UK animation</h2>
+        <h2>studio & production company</h2>
+        
+        <div className="p-container">
+          <p className='first-p'>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
+          <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
+          <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
+        </div>
 
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
-
-
-
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
-
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
-
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
-
-
-
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
-
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
-
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
-
-
-
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
-
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
-
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
-
-
-
-
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
-
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
-
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
-
-
-
-
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
-
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
-
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
-
-
-
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
-
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
-
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
-
-
-
-        <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
-
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
-
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
-
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
-
-        <p>Get in touch with us HERE</p>
+        <h2 className='second-h2'>Got an idea or a script? Let’s</h2>
+        <h2>make it happen!</h2>
+        <p className="p-container">If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
+        <p className="p-container">Get in touch with us HERE</p>
       </div>
+
     </MiddleContainer>
-    <div>
-    <p>We're A+C, a UK animation studio & production company</p>  
-        <p>Website Main Page 05</p>
-        <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
+    <div style={{ width: '100%' }}>
+      <p>We're A+C, a UK animation studio & production company</p>  
+          <p>Website Main Page 05</p>
+          <p>As specialists in stop-motion and animation for advertising and social content, we produce anything animated - from paper-craft to motion design.</p>
 
-        <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
+          <p>For over 10 years we’ve been creating award-winning animations for some of the world’s best-known brands, including LEGO, British Airways and TK Maxx.</p>
 
-        <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
+          <p>We’re a fully equipped animation production company with our own crew and studio space, which makes your life easier and keeps production costs down.</p>
 
-        <p>Got an idea or a script? Let’s make it happen!</p>
-        <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
+          <p>Got an idea or a script? Let’s make it happen!</p>
+          <p>If you have a fully shaped animation brief, script, or even the start of an idea, we’re happy to chat. Our experienced production team are ready to run through your concept and help with scheduling, budgeting and technical queries.</p>
 
         <p>Get in touch with us HERE</p>
     </div>
--- src/Components/MiddleContent/styles.js
+++ src/Components/MiddleContent/styles.js
@@ -20,5 +20,41 @@ export const MiddleContainer = styled.div`
   .content {
     width: 50%;
     transform: translateX(100%);
+    min-height: 101vh;
+
+    h2 {
+      width: 50%;
+      background-color: white;
+      font-weight: 600;
+      color: #180f59;
+      z-index: 100;
+      position: relative;
+      padding: .25rem 1.5rem;
+      line-height: 1.2;
+      border-radius: 500px;
+      transform: translateX(-100px);
+      font-size: 3.5833333333rem;
+      white-space: nowrap;
+      width: fit-content;
+    }
+
+    p {
+      color: #180f59;
+      font-size: 1.11rem;
+      margin-top: 20px;
+    }
+
+    .first-p {
+      font-size: 1.438rem;
+      margin-top: 50px;
+    }
+
+    .p-container {
+      padding: 0px 90px 73px;
+    }
+
+    .second-h2 {
+      margin-top: 100px;
+    }
   }
 `;
